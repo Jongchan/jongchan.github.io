65df168317efb6852216ba6c015f8754ed848da6
종찬 박 <JongchanPark@MacBook-Pro-2.local>
2017-02-22 17:07:07 +0900

    timeout to 20s

--- demo0/script.js
+++ demo0/script.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
 			contentType:"application/json",
             data: JSON.stringify(json_data),
             type: 'POST',
-			timeout: 10000,
+			timeout: 20000,
 			beforeSend: function(){
 				$("#processButton").isLoading();
 			},
